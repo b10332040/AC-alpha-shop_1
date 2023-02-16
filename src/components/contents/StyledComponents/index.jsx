4f7728aa232f223ecf91cc9941d30ece3c94f2ee
anna <b10332040@gmail.com>
2023-02-16 23:27:48 +0800

    [新增] StyledComponents 練習 styled-components

---
+++ src/components/contents/StyledComponents/index.jsx
@@ -0,0 +1,15 @@
+import { Rotate } from "./style";
+
+/**
+ * styled-components 學習
+ * @returns 
+ */
+function StyledComponents() {
+  return (
+    <div className="p-x-3">
+      <Rotate>&lt; 💅🏾 &gt;</Rotate>
+    </div>
+  );
+}
+
+export default StyledComponents;
